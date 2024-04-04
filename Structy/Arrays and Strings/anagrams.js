df7f3c77850bafb5f3f58e06d227fd83bf58812b
Samuel Kim <samuelskim420@gmail.com>
2024-04-04 21:01:49 +0000

    Anagrams

---
+++ Structy/Arrays and Strings/anagrams.js
@@ -0,0 +1,79 @@
+// Problem:
+// anagrams
+// Write a function, anagrams, that takes in two strings as arguments.
+// The function should return a boolean indicating whether or not the strings are anagrams.
+// Anagrams are strings that contain the same characters, but in any order.
+
+// Complexity: 
+// n = length of string 1
+// m = length of string 2
+// Time: O(n + m)
+// Space: O(n + m)
+
+// My Solution: 
+const anagrams = (s1, s2) => {
+    if (s1.length !== s2.length) return false;
+
+    const count = {}
+    for (let char of s1) {
+        if (!(char in count)) {
+            count[char] = 0
+        }
+        count[char]++
+    }
+
+    for (let char of s2) {
+        if (char in count) {
+            count[char]--
+        } else {
+            return false
+        }
+    }
+
+    let counters = Object.values(count);
+    for (let counter of counters) {
+        if (counter !== 0) return false;
+    };
+
+    return true;
+
+};
+
+// Sara's solution: 
+const anagrams = (s1, s2) => {
+    if (s1.length != s2.length) return false;
+
+    const hash = {};
+    for (let char of s1) {
+        char in hash ? hash[char]++ : hash[char] = 1
+    }
+
+    for (let char of s2) {
+        if (hash[char]) {
+            hash[char]--;
+
+        } else {
+            console.log(char)
+            console.log(hash)
+            console.log(hash[char])
+            return false;
+        }
+    }
+    return true;
+};
+
+
+
+// Test Cases:
+anagrams('restful', 'fluster'); // -> true
+anagrams('cats', 'tocs'); // -> false
+anagrams('monkeyswrite', 'newyorktimes'); // -> true
+anagrams('monkeyswrite', 'newyorktimes'); // -> true
+anagrams('monkeyswrite', 'newyorktimes'); // -> true
+anagrams('tax', 'taxi'); // -> false
+anagrams('tax', 'taxi'); // -> false
+anagrams('night', 'thing'); // -> true
+anagrams('night', 'thing'); // -> true
+anagrams('po', 'popp'); // -> false
+anagrams('pp', 'oo') // -> false
+
