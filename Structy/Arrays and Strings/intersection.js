7d2137d07693bf1effc0a8255a28aea44d8935b2
Samuel Kim <samuelskim420@gmail.com>
2024-04-10 23:50:46 -0400

    intersection start

---
+++ Structy/Arrays and Strings/intersection.js
@@ -0,0 +1,65 @@
+// Problem
+
+// intersection
+// Write a function, intersection, that takes in two arrays, a, b, as arguments.
+// The function should return a new array containing elements that are in both of the two arrays.
+// You may assume that each input array does not contain duplicate elements.
+
+// Complexities:
+
+// My Solution:
+
+// Structy's Solutions:
+
+// #1: Brute Force (Timeout):
+
+const intersection = (a, b) => {
+    const result = [];
+    for (let item of b) {
+        if (a.includes(item)) {
+            result.push(item);
+        }
+    }
+    return result;
+};
+
+// #2: Using Set (Pass):
+
+const intersection = (a, b) => {
+    const result = [];
+    const setA = new Set(a);
+    for (let item of b) {
+        if (setA.has(item)) {
+            result.push(item);
+        }
+    }
+    return result;
+};
+
+
+// Sara's Solution:
+
+const intersection = (a, b) => {
+    const set = new Set(a);
+    const res = [];
+    for (let num of b) {
+        set.has(num) ? res.push(num) : ""
+    }
+
+    return res
+}
+
+// Test Cases: 
+
+intersection([4, 2, 1, 6], [3, 6, 9, 2, 10]) // -> [2,6]
+intersection([2, 4, 6], [4, 2]) // -> [2,4]
+intersection([4, 2, 1], [1, 2, 4, 6]) // -> [1,2,4]
+intersection([0, 1, 2], [10, 11]) // -> []
+const a = [];
+const b = [];
+for (let i = 0; i < 50000; i += 1) {
+    a.push(i);
+    b.push(i);
+}
+intersection(a, b) // -> [0,1,2,3,..., 49999]
+
