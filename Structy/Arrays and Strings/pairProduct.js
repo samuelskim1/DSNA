f3f5bf1c8c6b253462b536fc95b4559ab070f79b
Samuel Kim <samuelskim420@gmail.com>
2024-04-11 00:22:02 -0400

    pairProduct test cases

--- Structy/Arrays and Strings/pairProduct.js
+++ Structy/Arrays and Strings/pairProduct.js
@@ -51,3 +51,14 @@ const pairProduct = (numbers, targetProduct) => {
         hash[number] = i;
     }
 };
+
+
+// Test Cases:
+
+pairProduct([3, 2, 5, 4, 1], 8); // -> [1, 3]
+pairProduct([3, 2, 5, 4, 1], 10); // -> [1, 2]
+pairProduct([4, 7, 9, 2, 5, 1], 5); // -> [4, 5]
+pairProduct([4, 7, 9, 2, 5, 1], 35); // -> [1, 4]
+pairProduct([3, 2, 5, 4, 1], 10); // -> [1, 2]
+pairProduct([4, 6, 8, 2], 16); // -> [2, 3]
+
