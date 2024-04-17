18acdd643b242cf587cd747fda55854878ccf20b
Samuel Kim <samuelskim420@gmail.com>
2024-04-17 14:36:54 -0400

    isPalindrome

---
+++ Tech Interviews/Antra/isPalindrome.js
@@ -0,0 +1,37 @@
+// Write a function to check if a given integer is a palindrome or not in Javascript
+// 121 -> true
+// 10 -> false
+// -1 -> false
+// 1234321 = > True
+// -1234321 - False
+// 1 -> True
+// 123321 -> True
+
+// Change integer to string
+// Iterate through string
+// Find the middle index
+// 
+
+const isPalidrome(integer) => {
+    if (integer < 0) return false;
+
+    const stringedInteger// (Change integer to string);
+
+    if (stringedInteger.length < 2) return true;
+
+    let middleIndex = stringedInteger.length / 2
+
+    let j = -1;
+
+    for (let i = 0; i < (stringedInteger.length / 2); i++) {
+        if (stringedInteger[i] === stringedInteger[j]) {
+            i++;
+            j--;
+        } else {
+            return false;
+        }
+    }
+
+
+
+}
