659e2d440450fa0e79b26203a111a7e5c4309dc4
Samuel Kim <samuelskim420@gmail.com>
2024-04-18 18:56:33 +0000

    finished fiveSort

--- Structy/Arrays and Strings/fiveSort.js
+++ Structy/Arrays and Strings/fiveSort.js
@@ -13,6 +13,22 @@
 
 // My Solution:
 
+const fiveSort = (nums) => {
+    let i = 0;
+    let j = nums.length - 1;
+    while (i < j) {
+        if (nums[j] === 5) {
+            j -= 1;
+        } else if (nums[i] === 5) {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            i += 1;
+        } else {
+            i += 1;
+        }
+    }
+
+    return nums;
+};
 
 // Structy's Solution: 
 
