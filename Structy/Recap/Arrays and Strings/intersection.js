b9654ccb0ce6253ae31ded2aefc566f6efbdc39a
Samuel Kim <samuelskim420@gmail.com>
2024-05-03 22:37:07 -0400

    test cases

--- Structy/Recap/Arrays and Strings/intersection.js
+++ Structy/Recap/Arrays and Strings/intersection.js
@@ -7,3 +7,18 @@ const intersection = (a, b) => {
 
     return res
 }
+
+// Test Cases: 
+
+intersection([4, 2, 1, 6], [3, 6, 9, 2, 10]) // -> [2,6]
+intersection([2, 4, 6], [4, 2]) // -> [2,4]
+intersection([4, 2, 1], [1, 2, 4, 6]) // -> [1,2,4]
+intersection([0, 1, 2], [10, 11]) // -> []
+const a = [];
+const b = [];
+for (let i = 0; i < 50000; i += 1) {
+    a.push(i);
+    b.push(i);
+}
+intersection(a, b) // -> [0,1,2,3,..., 49999]
+
