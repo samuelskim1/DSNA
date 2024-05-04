ccfeff9c7e96c98841e47f320fbfb8314082a747
Samuel Kim <samuelskim420@gmail.com>
2024-05-03 22:38:08 -0400

    pairSum review testcases

--- Structy/Recap/Arrays and Strings/pairSum.js
+++ Structy/Recap/Arrays and Strings/pairSum.js
@@ -11,3 +11,11 @@ const pairSum = (numbers, targetSum) => {
         hash[number] = i
     }
 };
+
+// Test Cases:
+pairSum([3, 2, 5, 4, 1], 8); // -> [0, 2]
+pairSum([4, 7, 9, 2, 5, 1], 5); // -> [0, 5]
+pairSum([4, 7, 9, 2, 5, 1], 3); // -> [3, 5]
+pairSum([1, 6, 7, 2], 13); // -> [1, 2]
+pairSum([9, 9], 18); // -> [0, 1]
+pairSum([6, 4, 2, 8], 12); // -> [1, 3]
