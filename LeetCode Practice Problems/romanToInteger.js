d4d2805328848013ecd61fb5475b406315fc7f8e
Samuel Kim <samuelskim420@gmail.com>
2024-05-18 19:56:45 -0400

    romanToInteger solution #2

--- LeetCode Practice Problems/romanToInteger.js
+++ LeetCode Practice Problems/romanToInteger.js
@@ -43,6 +43,8 @@
 // It is guaranteed that s is a valid roman numeral in the range[1, 3999].
 
 
+// Solution #1:
+
 /**
  * @param {string} s
  * @return {number}
@@ -72,6 +74,35 @@ var romanToInt = function (s) {
             }
         }
         result = result + intVal;
+    }
+    return result;
+};
+
+//Solution #2:
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+    var dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    s = s.replace("IV", "IIII").replace("IX", "VIIII");
+    s = s.replace("XL", "XXXX").replace("XC", "LXXXX");
+    s = s.replace("CD", "CCCC").replace("CM", "DCCCC");
+
+    var result = 0;
+
+    for (var i = 0; i < s.length; i++) {
+        result += dict[s[i]];
     }
     return result;
 };
