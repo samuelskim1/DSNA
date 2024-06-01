ead42f3735000f65ad852aa2c35efbd3cae05099
Samuel Kim <samuelskim420@gmail.com>
2024-06-01 17:51:57 -0400

    transformArray sol #1

--- LeetCode Practice Problems/transformArray.js
+++ LeetCode Practice Problems/transformArray.js
@@ -30,3 +30,13 @@
 // 0 <= arr.length <= 1000
 //     - 109 <= arr[i] <= 109
 // fn returns a number
+
+//solution #1 (for loop):
+
+const map = function (arr, fn) {
+    const transformedArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        transformedArr[i] = fn(arr[i], i);
+    }
+    return transformedArr;
+};
