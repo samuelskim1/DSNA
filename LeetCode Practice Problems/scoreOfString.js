3ad312906557f0119851c3773255e43edad92b1a
Samuel Kim <samuelskim420@gmail.com>
2024-06-01 17:56:40 -0400

    scoreOfString sol

--- LeetCode Practice Problems/scoreOfString.js
+++ LeetCode Practice Problems/scoreOfString.js
@@ -19,3 +19,14 @@
 // 2 <= s.length <= 100
 // s consists only of lowercase English letters.
 
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const scoreOfString = (s) => {
+    let ans = 0;
+    for (let i = 1; i < s.length; i++) {
+        ans += Math.abs(s.charCodeAt(i - 1) - s.charCodeAt(i));
+    }
+    return ans;
+};
