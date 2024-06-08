d29c7472d8a1c539d92f9c4d148328b78957a11f
Samuel Kim <samuelskim420@gmail.com>
2024-06-07 21:50:19 -0400

    solution #1

--- LeetCode Practice Problems/longestCommonPrefix.js
+++ LeetCode Practice Problems/longestCommonPrefix.js
@@ -16,3 +16,18 @@
 // 0 <= strs[i].length <= 200
 // strs[i] consists of only lowercase English letters.
 
+const longestCommonPrefix = function (strs) {
+    if (strs.length === 0) {
+        return '';
+    }
+    let answer = strs[0];
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(answer) !== 0) {
+            answer = answer.substring(0, answer.length - 1);
+            if (answer === '') {
+                return '';
+            }
+        }
+    }
+    return answer;
+};
