ca78fbb530b6c0a6198b90900740928a861cefec
Samuel Kim <samuelskim420@gmail.com>
2024-06-07 21:56:30 -0400

    isomorphicString done

--- LeetCode Practice Problems/isomorphicString.js
+++ LeetCode Practice Problems/isomorphicString.js
@@ -19,3 +19,18 @@
 // t.length == s.length
 // s and t consist of any valid ascii character.
 
+var isIsomorphic = function (s, t) {
+
+    for (let i = 0; i < s.length; i++) {
+
+        if (s.indexOf(s[i], i + 1) !== t.indexOf(t[i], i + 1)) {
+
+            return false;
+        }
+    }
+    return true;
+};
+
+// For enquiry about what is indexOf method, go to MDN.
+
+// (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf)
