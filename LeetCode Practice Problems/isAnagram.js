d626feda992aa303e6ba24268e1ece1ab60b34b5
Samuel Kim <samuelskim420@gmail.com>
2024-06-07 22:25:14 -0400

    isAnagram review done'

--- LeetCode Practice Problems/isAnagram.js
+++ LeetCode Practice Problems/isAnagram.js
@@ -22,3 +22,17 @@
 
 // Follow up: What if the inputs contain Unicode characters ? How would you adapt your solution to such a case?
 
+const isAnagram = function (s, t) {
+    s = s.split("").sort()
+    t = t.split("").sort()
+
+    if (s.length !== t.length)
+        return false;
+
+    for (const i = 0; i < s.length; i++)
+        if (s[i] !== t[i])
+            return false;
+
+    return true;
+};
+//Please vote it. If you like it.
