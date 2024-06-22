b95ecc1d1a636165adb4175ee12efde70027b28d
Samuel Kim <samuelskim420@gmail.com>
2024-06-22 12:36:51 -0400

    sumNumbersRecursive

---
+++ Structy/Beginner Recursion/sumNumbersRecursive.js
@@ -0,0 +1,21 @@
+// Write a function sumNumbersRecursive that takes in an array of numbers and returns the sum of all the numbers in the array. 
+//All elements will be integers. 
+// Solve this recursively.
+
+// My Solution: 
+
+const sumNumbersRecursive = (numbers) => {
+    if (numbers.length === 0) return 0
+    return numbers.pop() + sumNumbersRecursive(numbers)
+};
+
+
+// Test Cases:
+
+sumNumbersRecursive([5, 2, 9, 10]); // -> 26
+sumNumbersRecursive([1, -1, 1, -1, 1, -1, 1]); // -> 1
+sumNumbersRecursive([]); // -> 0
+sumNumbersRecursive([1000, 0, 0, 0, 0, 0, 1]); // -> 1001
+sumNumbersRecursive([700, 70, 7]); // -> 777
+sumNumbersRecursive([-10, -9, -8, -7, -6, -5, -4, -3, -2, -1]); // -> -55
+sumNumbersRecursive([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]); // -> 0
