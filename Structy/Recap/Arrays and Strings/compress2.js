ce3079b497f56d68106318c616b5a068cf5d9d95
Samuel Kim <samuelskim420@gmail.com>
2024-07-20 18:30:48 -0400

    compress review pt1

---
+++ Structy/Recap/Arrays and Strings/compress2.js
@@ -0,0 +1,16 @@
+// Problem: 
+
+// compress
+// Write a function, compress, that takes in a string as an argument.
+// The function should return a compressed version of the string where consecutive occurrences of the same characters are compressed into the number of occurrences followed by the character.
+// Single character occurrences should not be changed.
+// 'aaa' compresses to '3a'
+// 'cc' compresses to '2c'
+// 't' should remain as 't'
+// You can assume that the input only contains alphabetic characters.
+
+// Complexities:
+
+// n = length of string
+// Time: O(n)
+// Space: O(n)
