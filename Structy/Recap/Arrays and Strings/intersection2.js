634588e742d42f96cd55597c559c86fb94cf156b
Samuel Kim <samuelskim420@gmail.com>
2024-08-01 01:24:21 +0000

    intersection review

---
+++ Structy/Recap/Arrays and Strings/intersection2.js
@@ -0,0 +1,20 @@
+// Problem
+
+// intersection
+// Write a function, intersection, that takes in two arrays, a, b, as arguments.
+// The function should return a new array containing elements that are in both of the two arrays.
+// You may assume that each input array does not contain duplicate elements.
+
+// Complexities:
+
+// For brute force (timeout)
+
+// n = length of array a, m = length of array b
+// Time: O(n * m)
+// Space: O(min(n, m))
+
+// For using set (pass):
+
+// n = length of array a, m = length of array b
+// Time: O(n + m)
+// Space: O(n)
