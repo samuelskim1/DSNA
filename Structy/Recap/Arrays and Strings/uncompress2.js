32428067f44f6263c3fed35c3ecce2038cfe4238
Samuel Kim <samuelskim420@gmail.com>
2024-08-01 01:17:53 +0000

    uncompress pt3

--- Structy/Recap/Arrays and Strings/uncompress2.js
+++ Structy/Recap/Arrays and Strings/uncompress2.js
@@ -43,6 +43,32 @@ const uncompress = (s) => {
 
 };
 
+const uncompress = (s) => {
+    let result = [];
+    const numbers = '0123456789';
+    let i = 0;
+    let j = 0;
+    while (j < s.length) {
+        if (numbers.includes(s[j])) {
+            j += 1;
+        } else {
+            const num = Number(s.slice(i, j));
+            for (let count = 0; count < num; count += 1) {
+                result.push(s[j]);
+            }
+            j += 1;
+            i = j;
+        }
+    }
+    return result.join('');
+};
+// n = number of groups
+// m = max num found in any group
+// Time: O(n*m)
+// Space: O(n*m)
+
+
+
 
 // Test Cases: 
 uncompress("2c3a1t"); // -> 'ccaaat'
