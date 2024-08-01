173464bf85a255ace4b3d9abf20d8ba4f4b02b99
Samuel Kim <samuelskim420@gmail.com>
2024-08-01 01:24:44 +0000

    intersection pt 2

--- Structy/Recap/Arrays and Strings/intersection2.js
+++ Structy/Recap/Arrays and Strings/intersection2.js
@@ -18,3 +18,16 @@
 // n = length of array a, m = length of array b
 // Time: O(n + m)
 // Space: O(n)
+
+
+// My Solution:
+
+const intersection = (a, b) => {
+    const result = [];
+    for (let item of b) {
+        if (a.includes(item)) {
+            result.push(item);
+        }
+    }
+    return result;
+};
