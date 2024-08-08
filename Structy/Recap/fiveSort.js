669d7a9df0553ea4f0b0d76cdd75a07cae61eb83
Samuel Kim <samuelskim420@gmail.com>
2024-08-08 22:13:23 +0000

    fiveSort p2

--- Structy/Recap/fiveSort.js
+++ Structy/Recap/fiveSort.js
@@ -10,3 +10,20 @@
 // n = array size
 // Time: O(n)
 // Space: O(1)
+
+const fiveSort = (nums) => {
+    let i = 0;
+    let j = nums.length - 1;
+    while (i < j) {
+        if (nums[j] === 5) {
+            j -= 1;
+        } else if (nums[i] === 5) {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            i += 1;
+        } else {
+            i += 1;
+        }
+    }
+
+    return nums;
+};
