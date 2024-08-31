c7c7f67665ad7a5fbaefaaeeed3a330509c74e7c
Samuel Kim <samuelskim420@gmail.com>
2024-08-31 17:55:15 -0400

    lCP sol

--- Structy/Recap/longestCommonPrefix.js
+++ Structy/Recap/longestCommonPrefix.js
@@ -20,3 +20,19 @@
 // 1 <= strs.length <= 200
 // 0 <= strs[i].length <= 200
 // strs[i] consists of only lowercase English letters.
+
+const longestCommonPrefix = function (strs) {
+    if (strs.length === 0) {
+        return '';
+    }
+    let ans = strs[0];
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(ans) !== 0) {
+            ans = ans.substring(0, ans.length - 1);
+            if (ans === '') {
+                return '';
+            }
+        }
+    }
+    return ans;
+};
