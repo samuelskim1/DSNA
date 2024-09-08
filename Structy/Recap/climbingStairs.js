3e4d0aba677ff7f47c3f679afa72e401785236be
Samuel Kim <samuelskim420@gmail.com>
2024-09-07 20:32:22 -0400

    climbingStairs pt 2

--- Structy/Recap/climbingStairs.js
+++ Structy/Recap/climbingStairs.js
@@ -29,3 +29,19 @@ const climbStairs = function (n) {
 
     return dp[n];
 };
+
+const climbStairs = function (n) {
+    if (n <= 3) return n;
+
+    let prev1 = 3;
+    let prev2 = 2;
+    let cur = 0;
+
+    for (let i = 3; i < n; i++) {
+        cur = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = cur;
+    }
+
+    return cur;
+};
