4715eb3be9f64820e934a85efb41443199efdcbc
Samuel Kim <samuelskim420@gmail.com>
2024-09-07 20:38:39 -0400

    lPS sol #1

--- Structy/Recap/longestPalindromicSubstring.js
+++ Structy/Recap/longestPalindromicSubstring.js
@@ -21,3 +21,31 @@
 
 // 1 <= s.length <= 1000
 // s consist of only digits and English letters.
+
+const check = function (s, i, j) {
+    while (i < j) {
+        if (s[i] !== s[j]) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+
+const longestPalindrome = function (s) {
+    const n = s.length;
+    let starting_index = 0;
+    let max_len = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            if (check(s, i, j)) {
+                if (j - i + 1 > max_len) {
+                    max_len = j - i + 1;
+                    starting_index = i;
+                }
+            }
+        }
+    }
+    return s.substring(starting_index, starting_index + max_len);
+}
