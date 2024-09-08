74f440e5c8c6fc414c96fdea8b0f4e937f67d214
Samuel Kim <samuelskim420@gmail.com>
2024-09-07 20:31:51 -0400

    climbingStairs pt 1

--- Structy/Recap/climbingStairs.js
+++ Structy/Recap/climbingStairs.js
@@ -2,8 +2,6 @@
 
 // Each time you can either climb 1 or 2 steps.In how many distinct ways can you climb to the top ?
 
-
-
 //     Example 1:
 
 // Input: n = 2
@@ -19,3 +17,15 @@
 // 1. 1 step + 1 step + 1 step
 // 2. 1 step + 2 steps
 // 3. 2 steps + 1 step
+
+const climbStairs = function (n) {
+    const dp = new Array(n + 1).fill(0);
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+};
