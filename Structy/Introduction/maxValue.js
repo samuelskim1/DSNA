8520ed0c62f731280e788dc819c0035ee74f43e2
Samuel Kim <samuelskim420@gmail.com>
2024-10-03 13:21:51 -0400

    maxValue

--- Structy/Introduction/maxValue.js
+++ Structy/Introduction/maxValue.js
@@ -1,46 +1,23 @@
-//Problem
 // max value
-// Write a function, maxValue, that takes in array of numbers as an argument.
-// The function should return the largest number in the array.
+// Write a function, maxValue, that takes in array of numbers as an argument.The function should return the largest number in the array.
+
 // Solve this without using any built-in array methods.
+
 // You can assume that the array is non - empty.
 
-//Complexity: 
-//n = length of array
-// Time: O(n)
-// Space: O(1)
 
-//my solution:
-
-const maxValue = (nums) => {
-    let largestNum = -Infinity;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] > largestNum) {
-            largestNum = nums[i];
-        }
-    }
-    return largestNum;
-};
-
-//structy solution:
-// const maxValue = (nums) => {
-//     let maximum = -Infinity;
-
-//     for (let num of nums) {
-//         if (num > maximum) maximum = num;
-//     }
-
-//     return maximum;
-// };
-
-//test cases:
-//maxValue([4, 7, 2, 8, 10, 9]); // -> 10
-// maxValue([10, 5, 40, 40.3]); // -> 40.3
-// maxValue([42]); // -> 42
-// maxValue([1000, 8]); // -> 1000
-// maxValue([1000, 8]); // -> 1000
-// maxValue([2, 5, 1, 1, 4]); // -> 5
-
-
-
+// test case 00:
+maxValue([4, 7, 2, 8, 10, 9]); // -> 10;
+// test case 1:
+maxValue([10, 5, 40, 40.3]); // -> 40.3;
+// test case 2:
+maxValue([-5, -2, -1, -11]); // -> -1;
+// test case 3:
+maxValue([42]); // -> 42;
+// test case 4:
+maxValue([1000, 8]); // -> 1000;
+// test case 5:
+maxValue([1000, 8, 9000]); // -> 9000;
+// test case 6:
+maxValue([2, 5, 1, 1, 4]); // -> 5;
 
