090e821c636495601d684c669dcf97b9ea10a2d5
Samuel Kim <samuelskim420@gmail.com>
2024-11-02 20:54:42 -0400

    solution 2 for loop

--- lengthofLastWord.js
+++ lengthofLastWord.js
@@ -44,3 +44,24 @@ const lengthOfLastWord = function (s) {
 
     return end - start;
 };
+
+// sol #2:
+const lengthOfLastWord = function (s) {
+    let length = 0;
+    let counting = false;
+
+    for (let c of s) {
+        if (c !== ' ') {
+            if (!counting) {
+                counting = true;
+                length = 1;
+            } else {
+                length++;
+            }
+        } else {
+            counting = false;
+        }
+    }
+
+    return length;
+};
