6f562ae5a30624ab4a3aaeab06d5e1baddfac281
Samuel Kim <samuelskim420@gmail.com>
2024-11-09 21:49:22 -0500

    intersection sol #3

--- intersection.js
+++ intersection.js
@@ -41,3 +41,30 @@ const intersection = (a, b) => {
     }
     return result;
 };
+
+// #3: Using Set (Pass):
+
+const intersection = (a, b) => {
+    const result = [];
+    const setA = new Set(a);
+    for (let item of b) {
+        if (setA.has(item)) {
+            result.push(item);
+        }
+    }
+    return result;
+};
+
+// Test Cases: 
+
+intersection([4, 2, 1, 6], [3, 6, 9, 2, 10]) // -> [2,6]
+intersection([2, 4, 6], [4, 2]) // -> [2,4]
+intersection([4, 2, 1], [1, 2, 4, 6]) // -> [1,2,4]
+intersection([0, 1, 2], [10, 11]) // -> []
+const a = [];
+const b = [];
+for (let i = 0; i < 50000; i += 1) {
+    a.push(i);
+    b.push(i);
+}
+intersection(a, b) // -> [0,1,2,3,..., 49999]
