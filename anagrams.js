5b0e61f4e284ea706c22dd549df5b5a0f5b4230a
Samuel Kim <samuelskim420@gmail.com>
2024-11-23 19:01:34 -0500

    structy review

---
+++ anagrams.js
@@ -0,0 +1,11 @@
+// Problem:
+// anagrams
+// Write a function, anagrams, that takes in two strings as arguments.
+// The function should return a boolean indicating whether or not the strings are anagrams.
+// Anagrams are strings that contain the same characters, but in any order.
+
+// Complexity: 
+// n = length of string 1
+// m = length of string 2
+// Time: O(n + m)
+// Space: O(n + m)
