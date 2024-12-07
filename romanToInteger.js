10da3a7741065b4f300486357a7400877db980b3
samuelskim1 <samuelskim420@gmail.com>
2024-12-07 16:44:20 -0500

    romanToIntger sol#1

--- romanToInteger.js
+++ romanToInteger.js
@@ -41,3 +41,38 @@
 // 1 <= s.length <= 15
 // s contains only the characters('I', 'V', 'X', 'L', 'C', 'D', 'M').
 // It is guaranteed that s is a valid roman numeral in the range[1, 3999].
+
+// Solution #1:
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+    var dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    var result = 0;
+    var intVal = 0;
+    var nextIntVal = 0;
+    for (var i = 0; i < s.length; i++) {
+        intVal = dict[s[i]];
+
+        if (i != s.length - 1) {
+            nextIntVal = dict[s[i + 1]];
+            if (nextIntVal > intVal) {
+                intVal = nextIntVal - intVal;
+                i = i + 1;
+            }
+        }
+        result = result + intVal;
+    }
+    return result;
+};
